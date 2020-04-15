aab5d81a6446cee72e925c4d153d7ea41393c38f
sebastian cornejo <sebastian.cornejo@gmail.com>
2020-04-15 18:35:28 -0400

    added transposed products

--- src/producto3_13_14.py
+++ src/producto3_13_14.py
@@ -73,6 +73,17 @@ if __name__ == '__main__':
     #print(cumulativoCasosNuevos.columns)
     #print(cumulativoCasosTotales.columns)
     print(cumulativoFallecidos.columns)
+    cumulativoCasosNuevos_T = cumulativoCasosNuevos.transpose()
+    cumulativoCasosTotales_T = cumulativoCasosTotales.transpose()
+    cumulativoFallecidos_T = cumulativoFallecidos.transpose()
+
     cumulativoCasosNuevos.to_csv('../output/producto13/CasosNuevosCumulativo.csv', index=False)
+    cumulativoCasosNuevos_T.to_csv('../output/producto13/CasosNuevosCumulativo_T.csv', header=False)
+
     cumulativoCasosTotales.to_csv('../output/producto3/CasosTotalesCumulativo.csv', index=False)
+    cumulativoCasosTotales_T.to_csv('../output/producto3/CasosTotalesCumulativo_T.csv', header=False)
+
     cumulativoFallecidos.to_csv('../output/producto14/FallecidosCumulativo.csv', index=False)
+    cumulativoFallecidos_T.to_csv('../output/producto14/FallecidosCumulativo_T.csv', header=False)
+
+
