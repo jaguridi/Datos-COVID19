a448bf94df7aacdfa2ca22c98d2836120eef4289
sebastian cornejo <sebastian.cornejo@gmail.com>
2020-04-17 18:35:51 -0400

    added nuevo producto5

--- src/webpage.py
+++ src/webpage.py
@@ -289,7 +289,7 @@ if __name__ == '__main__':
     prod5('https://www.minsal.cl/nuevo-coronavirus-2019-ncov/casos-confirmados-en-chile-covid-19/', '../output/producto5/')
 
 
-    prod5Nuevo('https://www.minsal.cl/nuevo-coronavirus-2019-ncov/casos-confirmados-en-chile-covid-19/', '../output/producto5/')
+    prod5Nuevo('https://www.minsal.cl/nuevo-coronavirus-2019-ncov/casos-confirmados-en-chile-covid-19/', '../output/producto5/TotalesNacionales.csv')
 
 
     print('Generando producto 11')
