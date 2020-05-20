fixed normalization and first udd colab product

src/registroCivil.py
@@ -135,7 +135,6 @@ def prod31_32(fte, prod):
 
     data.to_csv(prod + outputPrefix + '_std.csv', index=False)
 
-
     reshaped = pd.pivot_table(data, index=['Region', 'Comuna'], columns=['Fecha'], values=outputPrefix)
     reshaped.fillna(0, inplace=True)
     reshaped = reshaped.applymap(np.int64)
@@ -146,6 +145,7 @@ def prod31_32(fte, prod):
     data_t.index.rename('', inplace=True)
 
     data_t.to_csv(prod + outputPrefix + '_T.csv')
+
 
 def APIupdate(URL, prod):
     # check if we're on nacimientos or defunciones and when was the last update to the files
@@ -172,7 +172,6 @@ def APIupdate(URL, prod):
     lastDate = dt.datetime.strftime(lastDate, "%Y-%m-%d")
     now = dt.datetime.today().strftime("%Y-%m-%d")
     now_as_date = dt.datetime.strptime(now, "%Y-%m-%d")
-
 
 
     if (lastDate_as_date >= now_as_date):
@@ -243,18 +242,19 @@ def APIupdate(URL, prod):
                                          ])
 
         # normalize all on data, but test on df as it's smaller
-        dfaux = insertCodigoRegion(data)
-        if 'Nacimientos' in data.columns:
-            # Region,Comuna,Nacimientos,Fecha,Código Región,Nombre Región,Código Provincia,Nombre Provincia,Código Comuna 2017,Nombre Comuna
-            data = dfaux[['Nombre Región', 'Código Región', 'Nombre Comuna', 'Código Comuna 2017', 'Nacimientos', 'Fecha']].copy()
-
-        elif 'Defunciones' in data.columns:
-            data = dfaux[['Nombre Región', 'Código Región', 'Nombre Comuna', 'Código Comuna 2017', 'Defunciones', 'Fecha']].copy()
-
-        data.rename(columns={'Nombre Región': 'Region',
-                                 'Código Región': 'Codigo region',
-                                 'Nombre Comuna': 'Comuna',
-                                 'Código Comuna 2017': 'Codigo comuna'}, inplace=True)
+        # dfaux = normalizaNombreCodigoRegionYComuna(data)
+        data = normalizaNombreCodigoRegionYComuna(data)
+        # if 'Nacimientos' in data.columns:
+        #     # Region,Comuna,Nacimientos,Fecha,Código Región,Nombre Región,Código Provincia,Nombre Provincia,Código Comuna 2017,Nombre Comuna
+        #     data = dfaux[['Nombre Región', 'Código Región', 'Nombre Comuna', 'Código Comuna 2017', 'Nacimientos', 'Fecha']].copy()
+        #
+        # elif 'Defunciones' in data.columns:
+        #     data = dfaux[['Nombre Región', 'Código Región', 'Nombre Comuna', 'Código Comuna 2017', 'Defunciones', 'Fecha']].copy()
+        #
+        # data.rename(columns={'Nombre Región': 'Region',
+        #                          'Código Región': 'Codigo region',
+        #                          'Nombre Comuna': 'Comuna',
+        #                          'Código Comuna 2017': 'Codigo comuna'}, inplace=True)
 
         data.to_csv(prod + outputPrefix + '_std.csv', index=False)
 